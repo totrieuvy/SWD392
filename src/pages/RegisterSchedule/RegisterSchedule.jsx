0c96a8c631d73946b1d00f83a76bb5588554f3b0
Apple <ngochingokien@gmail.com>
2025-02-06 23:27:57 +0700

    Add RegisterSchedule page, paitent section , guardian section

---
+++ src/pages/RegisterSchedule/RegisterSchedule.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import PatientSection from '../../components/RegisterSchedule/PatientSection/PatientSection'
+import GuardianSection from '../../components/RegisterSchedule/GuardianSection/GuardianSection'
+
+export default function RegisterSchedule() {
+  return (
+    <>
+    <br/>
+      <PatientSection/>
+      <br/>
+      <GuardianSection/>
+    </>
+  
+  )
+}
